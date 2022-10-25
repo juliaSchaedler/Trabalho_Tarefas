b18a152b6ac1a16e7f975fe7b1569a36f224928a
Julia Schaedler <juliaschaedler@gmail.com>
2022-10-25 15:00:55 -0300

    primeira versao

---
+++ src/constants/index.js
@@ -0,0 +1,28 @@
+const COLORS ={
+    primary: "#F2E9E4",
+    secondary:"#D8BD8A",
+    accent: "#AA5042"
+}
+
+const SIZES ={
+    padding: 19,
+
+    borderRadius: 15,
+    textBoxRadius: 25,
+    h1: 24,
+    h2: 20
+}
+
+const FONTS ={
+    h1_semiBold: {fontSize: SIZES.h1},
+    h2_semiBold: {fontSize: SIZES.h2}
+}
+
+const SHADOW = {
+    elevation: 5,
+    shadowColor: COLORS.secondary,
+    shadowOffset: { width: 2, height: 12 },
+    shadowRadius: 12,
+}
+
+export {COLORS, SIZES, FONTS, SHADOW}
